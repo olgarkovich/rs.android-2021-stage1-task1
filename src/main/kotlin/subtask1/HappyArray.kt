51492f28e052701764d35e025db032cf2f8fe671
olgarkovich <olgagarkovich@gmail.com>
2021-05-21 22:26:06 +0300

    complete task 1-3

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,47 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        if (sadArray.isEmpty()) {
+            return sadArray
+        }
+
+        val currentArray = arrayListOf<Int>()
+        for (item in sadArray) {
+            currentArray.add(item)
+        }
+        var checkChanges = true
+        while (checkChanges) {
+            checkChanges = false
+            val resultArray = arrayListOf<Int>()
+            for (i in 0..currentArray.lastIndex) {
+                if (i == 0) {
+                    resultArray.add(currentArray[i])
+                } else if (i == currentArray.lastIndex) {
+                    resultArray.add(currentArray[i])
+                } else {
+                    if (currentArray[i] <= currentArray[i - 1] + currentArray[i + 1]) {
+                        resultArray.add(currentArray[i])
+                    } else {
+                        checkChanges = true
+                    }
+                }
+            }
+
+            if (checkChanges) {
+                currentArray.clear()
+                currentArray.addAll(resultArray)
+            }
+            else {
+                break
+            }
+        }
+
+        val result = IntArray(currentArray.size)
+        for (i in 0..currentArray.lastIndex) {
+            result[i] = currentArray[i]
+        }
+
+        return result
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,44 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val resultArray = arrayListOf<String>()
+        for (i in 0..inputString.lastIndex) {
+            when (inputString[i]) {
+                '<' -> resultArray.add(getSubstring(inputString, i, '<', '>'))
+                '[' -> resultArray.add(getSubstring(inputString, i, '[', ']'))
+                '(' -> resultArray.add(getSubstring(inputString, i, '(', ')'))
+            }
+        }
+
+        return resultArray.toTypedArray()
+    }
+
+    private fun getSubstring(
+        inputString: String,
+        i: Int,
+        openBracket: Char,
+        closeBracket: Char
+    ): String {
+        var str = ""
+        var checkRepeat = 0
+        for (k in i + 1..inputString.lastIndex) {
+            if (inputString[k] == openBracket) {
+                checkRepeat++
+            }
+
+            if (inputString[k] == closeBracket) {
+
+                if (checkRepeat > 0) {
+                    str += inputString[k]
+                    checkRepeat--
+                } else {
+                    return str
+                }
+            } else {
+                str += inputString[k]
+            }
+        }
+        return str
     }
 }
